94046e5eaa0eb962a2c6131ae4c7ba4ad5ab26af
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-06-08 12:44:03 -0400

    [UPDATE] (services barril) add services

--- src/api/factories/chatwoot.factory.js
+++ src/api/factories/chatwoot.factory.js
@@ -1,4 +1,4 @@
-import { Chatwoot } from '../services/chatwoot.service.js';
+import { Chatwoot } from '../services/index.js';
 import { chatwootConfig } from '../../config.js';
 
 export const chatwoot = new Chatwoot(chatwootConfig);
--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,2 +1,4 @@
 export { ChatwootFlow } from './chatwoot-flow.service.js'
 export { TypeBot } from './typebot.service.js'
+export { Tickets } from './tickets.service.js'
+export { Chatwoot } from './chatwoot.service.js'
