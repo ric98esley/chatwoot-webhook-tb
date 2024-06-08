a8b2d39e95f3bd1e98b13b41ec8ef2dd9a93cd96
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-06-08 12:04:27 -0400

    [FETCH] (help desk)

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { apiConfig } from '../../config.js';
 import chatwootRouter from './chatwoot/index.js';
+import helpDeskRouter from './help-desk/index.js';
 
 const router = Router();
 
@@ -9,6 +10,7 @@ function routerApi(app) {
   const baseUrl = `/api/${apiConfig.version}`;
   app.use(baseUrl , router);
   router.use('/chatwoot', chatwootRouter);
+  router.use('/help-desk', helpDeskRouter);
 
   return baseUrl
 }
